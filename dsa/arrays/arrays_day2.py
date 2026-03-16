3d1f415a4c9ea9539949d02d4a0f381b899dc9ed
Yash Raj <yashrocksraj@gmail.com>
2026-03-17 03:43:28 +0530

    updated arrays practice

--- dsa/arrays/arrays_day2.py
+++ dsa/arrays/arrays_day2.py
@@ -1,24 +1,26 @@
 # Problem 1
 # Find Second largest no.
-# arr = [4,7,2,9,5]
-# largest_num = arr[0]
-# second_largest_num = float('-inf')
-# for num in arr:
-#     if(num > largest_num ):
-#         second_largest_num = largest_num
-#         largest_num = num
-#     elif (num > second_largest_num and num != largest_num):
-#         second_largest_num = num
-# print("second largest num :",second_largest_num)            
+
+arr = [4,7,2,9,5]
+largest_num = arr[0]
+second_largest_num = float('-inf')
+for num in arr:
+    if(num > largest_num ):
+        second_largest_num = largest_num
+        largest_num = num
+    elif (num > second_largest_num and num != largest_num):
+        second_largest_num = num
+print("second largest num :",second_largest_num)            
 # #------------------------#
 
 # Problem 2
-# Reverse an Array
-# arr = [1,2,3,4,5]
-# rev_arr = []
-# for i in range(len(arr)-1,-1,-1):
-#     rev_arr.append(arr[i])
-# print(rev_arr)    
+# Reverse an Array.
+
+arr = [1,2,3,4,5]
+rev_arr = []
+for i in range(len(arr)-1,-1,-1):
+    rev_arr.append(arr[i])
+print(rev_arr)    
 # #------------------------#
 
 # Problem 3
@@ -29,4 +31,5 @@ nums_greater_5 = 0
 for num in arr:
     if num > 5:
       nums_greater_5 += 1
-print(nums_greater_5)   
+print(nums_greater_5)
+# #------------------------#   
