1513f43e40e0685dcd3495ba080694f5b3e57c55
Yash Raj <yashrocksraj@gmail.com>
2026-03-17 03:40:07 +0530

    Day 2 arrays practice

--- dsa/arrays/arrays_day2.py
+++ dsa/arrays/arrays_day2.py
@@ -1,12 +1,32 @@
 # Problem 1
 # Find Second largest no.
-arr = [4,7,2,9,5]
-largest_num = arr[0]
-second_largest_num = float('-inf')
+# arr = [4,7,2,9,5]
+# largest_num = arr[0]
+# second_largest_num = float('-inf')
+# for num in arr:
+#     if(num > largest_num ):
+#         second_largest_num = largest_num
+#         largest_num = num
+#     elif (num > second_largest_num and num != largest_num):
+#         second_largest_num = num
+# print("second largest num :",second_largest_num)            
+# #------------------------#
+
+# Problem 2
+# Reverse an Array
+# arr = [1,2,3,4,5]
+# rev_arr = []
+# for i in range(len(arr)-1,-1,-1):
+#     rev_arr.append(arr[i])
+# print(rev_arr)    
+# #------------------------#
+
+# Problem 3
+# Count nums greater than 5
+
+arr = [2,8,3,10,6]
+nums_greater_5 = 0
 for num in arr:
-    if(num > largest_num ):
-        second_largest_num = largest_num
-        largest_num = num
-    elif (num > second_largest_num and num != largest_num):
-        second_largest_num = num
-print("second largest num :",second_largest_num)            
+    if num > 5:
+      nums_greater_5 += 1
+print(nums_greater_5)   
--- python basics/loops_practice/range_loops_practice.py
+++ python basics/loops_practice/range_loops_practice.py
@@ -4,10 +4,10 @@
 
 
 #(QS 2)  Print numbers from 100 to 1.
-# for i in range(100,0,-1):
-#     print(i) 
+for i in range(100,0,-1):
+    print(i) 
 
 # (QS 3) Print the multiplication table of a number n.
-n=int(input("enter a no:"))
-for i in range(1,11):
-    print(n*i)
+# n=int(input("enter a no:"))
+# for i in range(1,11):
+#     print(n*i)
