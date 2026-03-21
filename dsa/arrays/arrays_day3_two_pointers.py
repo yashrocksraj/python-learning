a41a62943f1672c61765b14623b58ea704507338
Yash Raj <yashrocksraj@gmail.com>
2026-03-21 06:00:24 +0530

    Learn how to create functions and return values

---
+++ dsa/arrays/arrays_day3_two_pointers.py
@@ -0,0 +1,57 @@
+# Problem 1
+# Reverse array using Two pointers
+
+def reverse_arr(arr:list[int]):
+    left = 0
+    right = len(arr) -1
+    while left < right:
+        arr[left] , arr[right]  = arr[right],arr[left]
+        left += 1
+        right -= 1
+    return arr  
+  
+lst=[1,2,3,4,5]
+print(reverse_arr(lst))
+#---------------#--------------#
+
+# Problem 2
+# Check whether array is Palindrome or not
+
+def palindrome(nums:list[int]):
+    left = 0
+    right= len(nums) - 1
+    while left < right:
+        if nums[left] != nums[right]:
+           return False
+        left += 1
+        right -= 1
+    return True
+
+arr1 = [1,2,3,2,1]
+print(palindrome(arr1))
+#---------------#--------------#
+
+# Problem 3
+# Check for Pair Sum
+def pair_sum(nums:list[int],target:int):
+    left = 0
+    right = len(nums)-1
+    while left < right:
+        curr_sum = nums[left] + nums[right]
+        if curr_sum == target:
+            return nums[left],nums[right]
+        elif curr_sum < target:
+            left += 1
+        else:
+            right -= 1
+    return None
+lst1 = [1,2,3,4,6]
+result = pair_sum(lst1,6) 
+if result:
+    print("Pair found:",result)
+else:
+    print("No Pair found")    
+
+
+           
+      
---
+++ python_basics/functions_practice/functions_practice.py
@@ -0,0 +1,25 @@
+def hello():
+    "This is a Function"
+    print("hello")
+    print("Glad to meet you")
+
+
+hello()
+print("we are there")
+hello()
+
+# Function Parameters
+
+def hello2(s): # s - parameter name
+    print("Hello " + s)
+    print("glad to meet you")
+hello2("Yash")  # "Yash" - parameter value
+hello2("Siya")  
+
+def hello3(s,n):
+    greeting = "Hello {} ".format(s)
+    print(greeting*n)
+
+hello3("Yash",2)
+hello3("",1)
+hello3("siya",4)
